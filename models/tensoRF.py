224d32554bd6907045f6f510275ec559b3c2c649
VinZh22 <154440827+VinZh22@users.noreply.github.com>
2025-03-07 13:20:57 +0000

    Added Tucker Decomposition for project

--- models/tensoRF.py
+++ models/tensoRF.py
@@ -332,11 +332,8 @@ class TensorCP(TensorBase):
         return grad_vars
 
     def compute_densityfeature(self, xyz_sampled):
-
         coordinate_line = torch.stack((xyz_sampled[..., self.vecMode[0]], xyz_sampled[..., self.vecMode[1]], xyz_sampled[..., self.vecMode[2]]))
         coordinate_line = torch.stack((torch.zeros_like(coordinate_line), coordinate_line), dim=-1).detach().view(3, -1, 1, 2)
-
-
         line_coef_point = F.grid_sample(self.density_line[0], coordinate_line[[0]],
                                             align_corners=True).view(-1, *xyz_sampled.shape[:1])
         line_coef_point = line_coef_point * F.grid_sample(self.density_line[1], coordinate_line[[1]],
@@ -431,4 +428,146 @@ class TensorCP(TensorBase):
         total = 0
         for idx in range(len(self.app_line)):
             total = total + reg(self.app_line[idx]) * 1e-3
+        return total
+
+
+class TensorTucker(TensorBase):
+    def __init__(self, aabb, gridSize, device, **kargs):
+        super(TensorTucker, self).__init__(aabb, gridSize, device, **kargs)
+
+    def init_svd_volume(self, res, device):
+        self.core_tensor_density = torch.nn.Parameter(0.2 * torch.randn((self.density_n_comp[0], self.density_n_comp[0], self.density_n_comp[0]), device=device))
+        self.density_line = self.init_one_svd(self.density_n_comp[0], self.gridSize, 0.2, device)
+
+        self.core_tensor_app = torch.nn.Parameter(0.2 * torch.randn((self.app_n_comp[0], self.app_n_comp[0], self.app_n_comp[0]), device=device))
+        self.app_line = self.init_one_svd(self.app_n_comp[0], self.gridSize, 0.2, device)
+        print("app_dim", self.app_dim)
+        print("app_n_comp", self.app_n_comp)
+        self.basis_mat = torch.nn.Parameter(torch.randn((self.app_n_comp[0], self.app_dim), device=device))
+
+
+    def init_one_svd(self, n_component, gridSize, scale, device):
+        line_coef = []
+        for i in range(len(self.vecMode)):
+            vec_id = self.vecMode[i]
+            line_coef.append(
+                torch.nn.Parameter(scale * torch.randn((1, n_component, gridSize[vec_id], 1))))
+        return torch.nn.ParameterList(line_coef).to(device)  # size (3, 1, n_component, gridSize, rank)
+
+    def get_optparam_groups(self, lr_init_spatialxyz=0.02, lr_init_network=0.001):
+        grad_vars = [{'params': self.core_tensor_density, 'lr': lr_init_spatialxyz},
+            {'params': self.density_line, 'lr': lr_init_spatialxyz},
+            {'params': self.core_tensor_app, 'lr': lr_init_spatialxyz},
+            {'params': self.app_line, 'lr': lr_init_spatialxyz},
+            {'params': self.basis_mat, 'lr':lr_init_network}]
+        if isinstance(self.renderModule, torch.nn.Module):
+            grad_vars += [{'params': self.renderModule.parameters(), 'lr': lr_init_network}]
+        return grad_vars
+
+
+    def compute_densityfeature(self, xyz_sampled):
+        """
+        Compute the density feature at the sampled points
+        :param xyz_sampled: Sampled points in the scene
+        :return: Density feature at the sampled points
+        """
+        coordinate_line = torch.stack((xyz_sampled[..., self.vecMode[0]], xyz_sampled[..., self.vecMode[1]], xyz_sampled[..., self.vecMode[2]]))
+        coordinate_line = torch.stack((torch.zeros_like(coordinate_line), coordinate_line), dim=-1).detach().view(3, -1, 1, 2)  #shape (3, N_point, 1, 2)
+        # Compute interpolated factor values
+        factor_x = F.grid_sample(self.density_line[0], coordinate_line[[0]],
+                                            align_corners=True).view(-1, *xyz_sampled.shape[:1])
+        factor_y = F.grid_sample(self.density_line[1], coordinate_line[[1]],
+                                        align_corners=True).view(-1, *xyz_sampled.shape[:1])
+        factor_z = F.grid_sample(self.density_line[2], coordinate_line[[2]],
+                                        align_corners=True).view(-1, *xyz_sampled.shape[:1])
+        # Perform Tucker tensor contraction to get density feature
+        density_feature = torch.einsum('ijk,ib,kb,jb->b', self.core_tensor_density, factor_x, factor_y, factor_z)
+        return density_feature
+
+    def compute_appfeature(self, xyz_sampled):
+        # Compute interpolated factor values
+
+
+        coordinate_line = torch.stack((xyz_sampled[..., self.vecMode[0]], xyz_sampled[..., self.vecMode[1]], xyz_sampled[..., self.vecMode[2]]))
+        coordinate_line = torch.stack((torch.zeros_like(coordinate_line), coordinate_line), dim=-1).detach().view(3, -1, 1, 2)  #shape (3, N_point, 1, 2)
+        # Compute interpolated factor values
+        factor_x = F.grid_sample(self.app_line[0], coordinate_line[[0]],
+                                            align_corners=True).view(-1, *xyz_sampled.shape[:1])
+        factor_y = F.grid_sample(self.app_line[1], coordinate_line[[1]],
+                                        align_corners=True).view(-1, *xyz_sampled.shape[:1])
+        factor_z = F.grid_sample(self.app_line[2], coordinate_line[[2]],
+                                        align_corners=True).view(-1, *xyz_sampled.shape[:1])
+        # Perform Tucker tensor contraction to get density feature
+        app_feature = torch.einsum('ijk, ib, kb, jb, in->bn', self.core_tensor_app, factor_x, factor_y, factor_z, self.basis_mat)
+        return app_feature
+
+    @torch.no_grad()
+    def up_sampling_Vector(self, density_line_coef, app_line_coef, res_target):
+
+        for i in range(len(self.vecMode)):
+            vec_id = self.vecMode[i]
+            density_line_coef[i] = torch.nn.Parameter(
+                F.interpolate(density_line_coef[i].data, size=(res_target[vec_id], 1), mode='bilinear', align_corners=True))
+            app_line_coef[i] = torch.nn.Parameter(
+                F.interpolate(app_line_coef[i].data, size=(res_target[vec_id], 1), mode='bilinear', align_corners=True))
+
+        return density_line_coef, app_line_coef
+
+    @torch.no_grad()
+    def upsample_volume_grid(self, res_target):
+        print(f'upsamping to {res_target}')
+        self.density_line, self.app_line = self.up_sampling_Vector(self.density_line, self.app_line, res_target)
+
+        self.update_stepSize(res_target)
+    
+
+    @torch.no_grad()
+    def shrink(self, new_aabb):
+        print("====> shrinking ...")
+        xyz_min, xyz_max = new_aabb
+        t_l, b_r = (xyz_min - self.aabb[0]) / self.units, (xyz_max - self.aabb[0]) / self.units
+
+        t_l, b_r = torch.round(torch.round(t_l)).long(), torch.round(b_r).long() + 1
+        b_r = torch.stack([b_r, self.gridSize]).amin(0)
+
+
+        for i in range(len(self.vecMode)):
+            mode0 = self.vecMode[i]
+            print("density_line", self.density_line[i].shape)
+            self.density_line[i] = torch.nn.Parameter(
+                self.density_line[i].data[...,t_l[mode0]:b_r[mode0],:]
+            )
+            print("density_line", self.density_line[i].shape)
+            self.app_line[i] = torch.nn.Parameter(
+                self.app_line[i].data[...,t_l[mode0]:b_r[mode0],:]
+            )
+
+        if not torch.all(self.alphaMask.gridSize == self.gridSize):
+            t_l_r, b_r_r = t_l / (self.gridSize-1), (b_r-1) / (self.gridSize-1)
+            correct_aabb = torch.zeros_like(new_aabb)
+            correct_aabb[0] = (1-t_l_r)*self.aabb[0] + t_l_r*self.aabb[1]
+            correct_aabb[1] = (1-b_r_r)*self.aabb[0] + b_r_r*self.aabb[1]
+            print("aabb", new_aabb, "\ncorrect aabb", correct_aabb)
+            new_aabb = correct_aabb
+
+        newSize = b_r - t_l
+        self.aabb = new_aabb
+        self.update_stepSize((newSize[0], newSize[1], newSize[2]))
+
+    def density_L1(self):
+        total = 0
+        for idx in range(len(self.density_line)):
+            total = total + torch.mean(torch.abs(self.density_line[idx]))
+        return total
+
+    def TV_loss_density(self, reg):
+        total = 0
+        for idx in range(len(self.density_line)):
+            total = total + reg(self.density_line[idx]) * 1e-3
+        return total
+
+    def TV_loss_app(self, reg):
+        total = 0
+        for idx in range(len(self.app_line)):
+            total = total + reg(self.app_line[idx]) * 1e-3        
         return total
